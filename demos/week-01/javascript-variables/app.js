8bf84aa11b048ce762141aab42d44d5431636bfa
Tim <tim.smith2814@gmail.com>
2024-07-03 09:30:13 +0100

    JS variables

---
+++ demos/week-01/javascript-variables/app.js
@@ -0,0 +1,16 @@
+// this is a comment. This code won't be executed
+// we use comment to describe what we are doing for future devs to read
+
+// here I am putting the string "Hello world" into the console
+console.log("Hello world");
+
+// VARIABLES
+
+// a variable is a "bucket" that we can put information into
+// each variable declaration looks like a setence
+// hey JS, let my name be equal to Tim please
+let myName = "Tim";
+
+// we can use variables over and over again
+console.log(myName); // notice how we don't put "" around the variable name
+console.log("myName"); // here JavaScript just gives us the string "myName", not the variables value
