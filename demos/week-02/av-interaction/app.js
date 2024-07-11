567a5aec819c2e1fd6562caa16d6016d3e27ddaa
Tim <tim.smith2814@gmail.com>
2024-07-11 10:03:27 +0100

    Add image optimisation demo

--- demos/week-02/av-interaction/app.js
+++ demos/week-02/av-interaction/app.js
@@ -6,9 +6,17 @@ image.addEventListener("load", function () {
 
 // Get infomration about the audio tag
 const audio = document.querySelector("audio");
+const timeElement = document.getElementById("timeElapsed");
 
 audio.addEventListener("timeupdate", function () {
   console.log(audio.currentTime);
+  // show time on the page
+  timeElement.textContent = formatTime(audio.currentTime);
+  // SUDO CODE
+  // updating a slider to show time elapsed
+  // assuming the audio is 100 seconds long
+  // i want to put the slider 1 point each second
+  // BUT its not always going to be 100 seconds, so I need to do maths to get the correct step for each second
 });
 
 // Making our own controls
@@ -41,3 +49,10 @@ volumeSlider.addEventListener("change", function (event) {
 // decrease time by 15 second (skip backwards)
 // if we had an array of mp3 names, we could change the src of our audio to be the next item in the array
 // there is also an "ended" event we could use to automatically play a new song
+
+// format time
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
