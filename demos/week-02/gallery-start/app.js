669d12eb483b64a8728ef71cda06c26d5361c7a3
Tim <tim.smith2814@gmail.com>
2024-07-11 13:21:35 +0100

    Gallery Start demo

---
+++ demos/week-02/gallery-start/app.js
@@ -0,0 +1,16 @@
+let currentIndex = 0;
+const images = [
+  { src: "hills.jpg", alt: "Beautiful green hills" }, // 0
+  { src: "stairs.jpg", alt: "Beautiful artistic stairs" }, // 1
+];
+
+function changeBG() {
+  // set the main image src to be images[currentIndex].src
+}
+
+// we need to put a small version of each of those images on the page in the #thumbnails div
+
+// each of those images needs an event listener so when we click it
+// it changes the main-image
+
+// we need a next and previous button to change to the next or previous image
