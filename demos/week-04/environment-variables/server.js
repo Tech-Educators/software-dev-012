fbbae130b197660ffdf72c3ab41a707891190b47
Tim <tim.smith2814@gmail.com>
2024-07-23 09:22:08 +0100

    Add environemnt varibales

--- demos/week-04/environment-variables/server.js
+++ demos/week-04/environment-variables/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 
-const superSecretThing = "Tim loves Jez";
+import dotenv from "dotenv"; // import the dotenv packacke
+dotenv.config(); // run a function that "sets up" the environment variables
+const superSecretThing = process.env.SUPER_SECRET_KEY; // get the variable value from .env
 
 app.get("/", function (request, response) {
   console.log(superSecretThing);
