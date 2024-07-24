b442ee7d43b6ae99360631075bc6a12cca2b8bb9
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-07-24 15:09:31 +0100

    databases

--- demos/week-04/postgres-intro/client/main.js
+++ demos/week-04/postgres-intro/client/main.js
@@ -1,5 +1,15 @@
 const container = document.getElementById('app')
 
+// fetch the games from from my frontend.
+
+fetchGames()
+async function fetchGames() {
+  // talk to the server and ask for the array of games
+  const result = await fetch(`http://localhost:8080/games`)
+  // how to read the incoming data
+  const games = await result.json()
+  generateImages(games)
+}
 
 function generateImages(array) {
   for (let i = 0; i < array.length; i++) {
@@ -8,5 +18,3 @@ function generateImages(array) {
     container.appendChild(img)
   }
 }
-
-generateImages(games)
--- demos/week-04/postgres-intro/server/server.js
+++ demos/week-04/postgres-intro/server/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import pg from "pg"
 
 const app = express()
 
@@ -8,8 +9,20 @@ app.use(express.json())
 app.use(cors())
 dotenv.config()
 
+// set up the database so we can query it. 
+const db = new pg.Pool({
+    connectionString: process.env.DATABASE_CONNECTION
+})
+
 app.get('/', (req, res) => {
     res.send(`You've reached the route route - please leave a message`)
+})
+
+app.get('/games', async function (request, response) {
+    // we can use the 'DB' object to query/talk to mypost database. 
+    const result = await db.query(`SELECT * FROM games`)
+    const games = result.rows
+    response.json(games)
 })
 
 app.listen('8080', () => {
